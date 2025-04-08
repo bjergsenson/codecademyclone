21ea2d3b2ac4535a941cdd731f2ad8d9746628b8
Manuel Alejandro Londoño Cadavid <malondonoc@gmail.com>
2025-04-07 20:16:41 -0500

    Add hardcoded lessons examples

---
+++ my-app/app/data/lessons.ts
@@ -0,0 +1,12 @@
+export const lessons = {
+    python: {
+      title: "Intro to Python",
+      content: "Print 'Hello World'",
+      starterCode: `print("Hello, world!")`,
+    },
+    js: {
+      title: "Intro to JS",
+      content: "Write your first function",
+      starterCode: `function greet() {\n  console.log("Hello!");\n}`,
+    },
+  };
